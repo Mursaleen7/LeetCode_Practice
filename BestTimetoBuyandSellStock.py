34514355d6605e8a4b0ab2c57c6c94920fab3d9b
Mursaleen <77685569+Mursaleen7@users.noreply.github.com>
2025-09-27 03:11:15 -0400

    Create BestTimetoBuyandSellStock.py

---
+++ BestTimetoBuyandSellStock.py
@@ -0,0 +1,19 @@
+class Solution1(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        num = float('inf')
+        i = 0
+        while i < len(prices):
+            j = i
+            while j < len(prices):
+                diff = prices[i] - prices[j]
+                if diff < num and diff < 0:
+                    num = diff
+                j += 1
+            i += 1
+        if num == float('inf'):
+            num = 0
+        return -num
