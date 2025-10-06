38e4fed9ae87a8a6d8379973d24c19c217743033
Mursaleen <77685569+Mursaleen7@users.noreply.github.com>
2025-10-06 16:45:53 -0400

    Create TwoSumII-InputArrayIsSorted.py

---
+++ TwoSumII-InputArrayIsSorted.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        sum=0
+        i=0
+        j=len(numbers)-1
+        while i<j:
+            sum=numbers[i]+numbers[j]
+            if sum < target:
+                i+=1
+            elif sum > target:
+                j-=1
+            else:
+                return i+1, j+1
+        return 0,0
+            
+                
